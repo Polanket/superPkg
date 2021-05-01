c39c5422dc6c6ea0ac2209f5c42c07cb60fe091a
Juan Polanco <juanmpt@outlook.com>
2021-05-01 13:45:25 +0200

    Refactor and remove unused code

--- superPkg/lib.py
+++ superPkg/lib.py
@@ -3,11 +3,9 @@ from art import *
 
 def type_it(text=None):
     if type(text) == list:
-        print(type(text), text)
         org_art = text2art(str(' '.join(text)), font='random')
         print(org_art)
     else:
-        print(type(text), text)
         example_art = text2art('Type something!', font='random')
         print(example_art)
 
